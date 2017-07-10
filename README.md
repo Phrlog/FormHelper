d5c9e644f3ddbfb58aa2533417a0f01f3b1064a2
phrlog <kh@linkprofit.com>
2017-07-10 14:46:22 +0300

    update readme and index.php

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/Phrlog/FormHelper.svg?branch=master)](https://travis-ci.org/Phrlog/FormHelper)
 # FormHelper
 Helper for formatting different data
--- index.php
+++ index.php
@@ -1,28 +1,21 @@
 <?php
 require_once 'vendor/autoload.php';
 
-//$phone_helper = new \FormHelper\PhoneHelper();
-//$passport_helper = new \FormHelper\PassportHelper();
-//$name_helper = new \FormHelper\FullNameHelper();
-//
-//$phone = $phone_helper->set('+7(985)829-00-42')
-//                ->removePhoneSpecifications()
-//                ->formatPhoneLength(10)
-//                ->get();
-//
-//$ua_passport = $passport_helper->set('АК654321')
-//                ->setLanguage('ua')
-//                ->explodePassport()
-//                ->get();
-//
-//list($pass_src, $pass_pass_num) = $passport_helper->set('1234198689')
-//                                    ->setLanguage('ru')
-//                                    ->explodePassport()
-//                                    ->get();
-//
-//$name = $name_helper->set('Балабанов Максим Григорьевич')
-//                ->isLatin()
-//                ->explodeName()
-//                ->get();
-//
-//var_dump($name);
+$phone_helper = new \FormHelper\PhoneHelper();
+$passport_helper = new \FormHelper\PassportHelper();
+$name_helper = new \FormHelper\FullNameHelper();
+
+$phone = $phone_helper->set('+7(985)829-00-42')
+                ->removePhoneSpecifications()
+                ->formatPhoneLength(10)
+                ->get();
+
+$ua_passport = $passport_helper->set('АК654321')
+                ->setLanguage('ua')
+                ->explodePassport()
+                ->get();
+
+$name = $name_helper->set('Балабанов Максим Григорьевич')
+                ->isLatin()
+                ->explodeName()
+                ->get();
