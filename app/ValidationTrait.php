8908f1f6b9fb31813fca325b74eed495d3a46075
phrlog <kh@linkprofit.com>
2017-07-10 16:33:11 +0300

    add validation trait

---
+++ app/ValidationTrait.php
@@ -0,0 +1,45 @@
+<?php
+namespace FormHelper;
+
+/**
+ * Class ValidationTrait
+ * @package FormHelper
+ */
+trait ValidationTrait
+{
+    /**
+     * @param $word
+     * @return bool
+     */
+    public static function isCyrillic($word)
+    {
+        return (bool) preg_match("/[\p{Cyrillic}]/ui", $word);
+    }
+
+    /**
+     * @param $word
+     * @return bool
+     */
+    public static function isLatin($word)
+    {
+        return (bool) preg_match("/[\p{Latin}]/ui", $word);
+    }
+
+    /**
+     * @param $word
+     * @return bool
+     */
+    public static function isText($word)
+    {
+        return (bool) preg_match("/(\p{Latin}+|[\p{Cyrillic}]+)$/u", $word);
+    }
+
+    /**
+     * @param $digit
+     * @return bool
+     */
+    public static function isInteger($digit)
+    {
+        return (bool) preg_match("/^\d+$/", $digit);
+    }
+}
